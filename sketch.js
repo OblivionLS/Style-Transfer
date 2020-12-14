0070749a1c7470fbf97236f3b26649a3693f3b91
OblivionLS <55786167+OblivionLS@users.noreply.github.com>
2020-12-14 11:12:28 +0100

    restoring Style Transfer

--- sketch.js
+++ sketch.js
@@ -10,6 +10,9 @@ This uses a pre-trained model of The Great Wave off Kanagawa and Udnie (Young Am
 === */
 
 let style;
+let style1;
+let style2;
+let style3;
 let video;
 let resultImg;
 let isTransferring = false;
@@ -17,7 +20,7 @@ let gotImage = false;
 
 function setup() {
 
-  createCanvas(900,500).parent('canvasContainer');
+  createCanvas(1000,600).parent('canvasContainer');
 
   video = createCapture(VIDEO);
   //video.size(100, 100);
@@ -29,42 +32,87 @@ function setup() {
 
   // Create a new Style Transfer method with a defined style.
   // We give the video as the second argument
-  style = ml5.styleTransfer('/models/mathura', video, modelLoaded);
+  style1 = ml5.styleTransfer('/models/fauvism', video, modelLoaded);
+  style2 = ml5.styleTransfer('/models/udnie', video, modelLoaded);
+ style3 = ml5.styleTransfer('/models/mathura', video, modelLoaded);
+  select('#startStop').mousePressed(start);
 
-  select('#startStop').mousePressed(startStop);
+  select('#style1').mousePressed(styleOne);
+  select('#style2').mousePressed(styleTwo);
+  select('#style3').mousePressed(styleThree);
 
 }
 
 function draw(){
   if(gotImage){
-    image(resultImg, 0, 0, 900, 500);
+    image(resultImg, 0, 0, 1000, 600);
   }else{
-    image(video, 0, 0, 900, 500);
+    image(video, 0, 0, 1000, 600);
   }
+
+}
+
+function styleOne(){
+  style = style1;
+  select('#status').html('Changed Style to nr 1');
+}
+
+function styleTwo(){
+  style = style2;
+  select('#status').html('Changed Style to nr 2');
+}
+
+function styleThree(){
+  style = style3;
+  select('#status').html('Changed Style to nr 3');
+}
+
+
+function start(){
+  if(style == null){
+    select('#startStop').html('You must choose a Style first');
+  } else{
+    if(!isTransferring){
+      document.getElementById("loading").style.opacity = 1;
+      select('#startStop').html('Paint me again!');
+
+
+        setTimeout(startStop, 10);
+    }else {
+      document.getElementById("loading").style.opacity = 0;
+
+      setTimeout(startStop, 10);
+    }
+
+  }
+
 
 }
 
 function startStop() {
 isTransferring = !isTransferring;
   if (!isTransferring) {
-    select('#startStop').html('Start');
+    select('#startStop').html('Start painting me!');
+    gotImage = false;
   } else {
-    select('#startStop').html('Stop');
+    select('#startStop').html('Paint me again!');
     // Make a transfer using the video
     style.transfer(gotResult);
-    gotImage = true;
     console.log("got an image");
   }
 }
 
 // A function to call when the model has been loaded.
 function modelLoaded() {
-  select('#status').html('Model Loaded');
+  select('#status').html('Choose Your Style!');
 }
 
 // When we get the results, update the result image src
 function gotResult(err, img) {
   resultImg.attribute('src', img.src);
+
+  document.getElementById("loading").style.opacity = 0;
+  gotImage = true;
 //  if (isTransferring) {
   //  style.transfer(gotResult);
   //}
